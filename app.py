b617c1f355fb0c387f2cd3136c053ae9068d344e
alsomeb <alsomeb@gmail.com>
2021-12-07 09:21:15 +0100

    upd app

---
+++ app.py
@@ -0,0 +1,15 @@
+import pygame     #import the module
+from led import mlcdinit,mlcddraw
+
+mlcdinit(16,3,3) # initialize a 16x3 display scaled 3x  
+
+#draw the three lines passed as a list
+mlcddraw(["Hello","Python", "What"])
+antalSeks = 0
+pygame.time.set_timer(pygame.USEREVENT, 1000)
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.USEREVENT:
+            antalSeks = antalSeks +1
+            mlcddraw(["Räknare","", str(antalSeks)])
+       
